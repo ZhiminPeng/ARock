26d98b05d2aab032b4f36bbc5c7a8ce714d40abe
Zhimin Peng <zhiminp@gmail.com>
2015-08-11 21:39:57 -0700

    format

--- src/test_logistic.cc
+++ src/test_logistic.cc
@@ -1,13 +1,13 @@
 /***********************************************************************
+ * Test ARock for solving l1 or l2 regularized logistic regression
  *
- * Main function to test ARock for solving l1 or l2
- *  regularized logistic regression
+ * For l1 regularized, we solve the following problem
  *
- * l2 regularized:
- *    min lambda/2 |x|_2^2 + 1/m log(1 + exp(-b_i* a_i * x) )
- *
- * l1 regularized:
  *    min lambda |x|_1 + 1/m log(1 + exp(-b_i* a_i * x) )
+ *
+ * For l2 regularized, we solve the following problem
+ * 
+ *    min lambda/2 |x|_2^2 + 1/m log(1 + exp(-b_i* a_i * x) )
  *
  * Date Created:  02/20/2015
  * Date Modified: 02/24/2015
@@ -17,7 +17,6 @@
  *                08/11/2015 (fixed the output format)
  * Author:        Zhimin Peng, Yangyang Xu, Ming Yan, Wotao Yin
  * Contact:       zhimin.peng@math.ucla.edu
- *
  **********************************************************************/
 
 #include <iostream>
@@ -38,11 +37,10 @@ void exit_with_help();
 void parse_input_argv(Parameters&, int, char**, std::string&, std::string&, int&);
 
 // main function
-int main ( int argc, char *argv[] ) {
-  // int thread_count = strtol ( argv[1], NULL, 10 );
+int main(int argc, char *argv[]) {
 
-  int n = 1;
-  int n_threads_to_use = 1;  
+  int n                     = 1;
+  int n_threads_to_use      = 1;  
   int max_n_threads_by_user = 2;
   
   /*************************
@@ -50,78 +48,80 @@ int main ( int argc, char *argv[] ) {
    *************************/
   Parameters para;
   std::string data_file_name;
-  std::string label_file_name;  
-  parse_input_argv ( para, argc, argv, data_file_name, label_file_name, max_n_threads_by_user );
+  std::string label_file_name;
+  parse_input_argv(para, argc, argv, data_file_name, label_file_name, max_n_threads_by_user);
   vector<vector<double> > result(max_n_threads_by_user, vector<double>(2));
 
   /**************************
     2. load data from file
    **************************/
+  // TODO: need a smart way to combine the sparse, and dense case together
+  // current, we have lots of duplicated code
   if (para.is_sparse) {
     SpMat A;
     Vector b;
-    loadMarket ( A, data_file_name );
-    loadMarket ( b, label_file_name );
+    loadMarket(A, data_file_name);
+    loadMarket(b, label_file_name);
     n = A.rows();
     // check the size of the data if match.
-    if ( b.size() != A.cols() ) {
-      std::cout<<"The size of A and b don't match!"<<endl;
+    if (b.size() != A.cols()) {
+      std::cout<<"The size of A and b don't match!"<<std::endl;
       return 0;
     }
-    Vector x ( n, 0. );
+    Vector x(n, 0.);
     
-    //----------------------------------------------------
     // start with asyn ls
-    std::cout<<"% start parallel ayn to solve "<<para.type<<" logistic regression!"<<endl;
+    std::cout << "% start parallel ayn to solve "
+              << para.type
+              << " logistic regression!"
+              << std::endl;
     
-    /*
-      ===========================
+    /****************************
       3. start the parallel test
-      ===========================
-    */
+     ****************************/
     int num_features = A.rows();
     int num_samples = A.cols();
 
-    std::cout<<"---------------------------------------------"<<endl;
-    std::cout<<"The problem has "<<num_samples<<" samples, "<<num_features<<" features."<<endl;
-    std::cout<<"The data matrix is sparse, "<<"lambda is: "<<para.lambda<<"."<<endl;
+    std::cout << "---------------------------------------------\n";
+    std::cout << "The problem has " << num_samples
+              << " samples, " << num_features << " features.\n";
+    std::cout << "The data matrix is sparse, "
+              << "lambda is: " << para.lambda << ".\n";
     
-    for ( n_threads_to_use = 1; n_threads_to_use <= max_n_threads_by_user; n_threads_to_use = n_threads_to_use * 2 ) {
-      Vector x ( n, 0. );
-      Vector Atx ( num_samples, 0. );
+    for (n_threads_to_use = 1; n_threads_to_use <= max_n_threads_by_user; n_threads_to_use = n_threads_to_use * 2) {
+      Vector x(n, 0.);
+      Vector Atx(num_samples, 0.);
       double start = omp_get_wtime();
-# pragma omp parallel num_threads ( n_threads_to_use ) shared ( A, b, x, Atx, para )
+# pragma omp parallel num_threads (n_threads_to_use) shared(A, b, x, Atx, para)
       {
-        if ( para.type == "l2" ) {
-          l2_logistic ( A, b, x, Atx, para );
+        if (para.type == "l2") {
+          l2_logistic(A, b, x, Atx, para);
         }
-        else if ( para.type == "l1" ) {
-          l1_logistic ( A, b, x, Atx, para );
+        else if (para.type == "l1") {
+          l1_logistic(A, b, x, Atx, para);
         }
       }
       double end = omp_get_wtime();
       
-      result[n_threads_to_use-1][0] = end - start;
-      if ( para.type == "l2" ) {
-        result[n_threads_to_use-1][1] = l2_objective ( A, b, x, Atx, para );
+      result[n_threads_to_use - 1][0] = end - start;
+      if (para.type == "l2") {
+        result[n_threads_to_use - 1][1] = l2_objective(A, b, x, Atx, para);
       }
-      if ( para.type == "l1" ) {
-        result[n_threads_to_use-1][1] = l1_objective ( A, b, x, Atx, para );
+      if (para.type == "l1") {
+        result[n_threads_to_use - 1][1] = l1_objective(A, b, x, Atx, para);
       }
     }
+  }  else {
 
-  }
-
-  if ( !para.is_sparse ) {
     Matrix A;
     Vector b;
-    loadMarket ( A, data_file_name );
-    loadMarket ( b, label_file_name );
+    loadMarket(A, data_file_name);
+    loadMarket(b, label_file_name);
     
     n = A.rows();
     // check the size of the data if match.
     if ( b.size() != A.cols() ) {
-      std::cout<<"The size of A and b don't match!"<<endl;
+      std::cout<<"The size of A and b don't match!"<<std::endl;
       return 0;
     }
     
@@ -129,7 +129,7 @@ int main ( int argc, char *argv[] ) {
     
     //----------------------------------------------------
     // start with asyn ls
-    std::cout<<"% start parallel ayn to solve "<<para.type<<" logistic regression!"<<endl;
+    std::cout<<"% start parallel ayn to solve "<<para.type<<" logistic regression!"<<std::endl;
     
     
     /*
@@ -212,39 +212,39 @@ void parse_input_argv ( Parameters& para,
                       std::string& label_file_name,
                       int& max_n_threads_by_user ) {
   for ( int i = 1; i < argc; ++i ) {
-      if ( argv[i][0] != '-' ) {
-          break;
-      }
-      if ( ++i >= argc ) {
+    if ( argv[i][0] != '-' ) {
+      break;
+    }
+    if ( ++i >= argc ) {
       exit_with_help();
-      }
-      if ( std::string ( argv[i-1] ) == "-type" ) {
+    }
+    if ( std::string ( argv[i-1] ) == "-type" ) {
       para.type = argv[i];
-      }
-      else if ( std::string ( argv[i-1] ) == "-lambda" ) {
+    }
+    else if ( std::string ( argv[i-1] ) == "-lambda" ) {
       para.lambda = atof ( argv[i] );
-      }
-      else if ( std::string ( argv[i-1] ) == "-is_sparse" ) {
+    }
+    else if ( std::string ( argv[i-1] ) == "-is_sparse" ) {
       para.is_sparse = atoi ( argv[i] );
-      }
-      else if ( std::string ( argv[i-1] ) == "-epoch" ) {
+    }
+    else if ( std::string ( argv[i-1] ) == "-epoch" ) {
       para.MAX_EPOCH = atoi ( argv[i] );
-      }
-      else if ( std::string ( argv[i-1] ) == "-data" ){
+    }
+    else if ( std::string ( argv[i-1] ) == "-data" ){
       data_file_name = std::string ( argv[i] );
-      }
-      else if ( std::string ( argv[i-1] ) == "-label" ) {
+    }
+    else if ( std::string ( argv[i-1] ) == "-label" ) {
       label_file_name = std::string ( argv[i] );
-      }
-      else if ( std::string ( argv[i-1] ) == "-nthread" ) {
+    }
+    else if ( std::string ( argv[i-1] ) == "-nthread" ) {
       max_n_threads_by_user = atoi ( argv[i] ) ;
-      }
-      else if ( std::string ( argv[i-1] ) == "-flag" ) {
+    }
+    else if ( std::string ( argv[i-1] ) == "-flag" ) {
       para.flag = atoi ( argv[i] );
-      }
-      else {
+    }
+    else {
       exit_with_help();
-      }
+    }
   }
   return;
 }
